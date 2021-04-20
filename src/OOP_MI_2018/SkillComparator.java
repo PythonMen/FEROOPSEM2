188da89cbd7e325a1396c097900bfeef15b55dc1
Hrvoje Cerin <hrvoje.cerin@gmail.com>
2021-04-20 10:08:58 +0200

    1. Zadatak b)

--- src/OOP_MI_2018/SkillComparator.java
+++ src/OOP_MI_2018/SkillComparator.java
@@ -2,10 +2,12 @@ public class SkillComparator implements Comparator {
 
     @Override
     public int compare(Object musician1, Object musician2){
-        Musician m1 = (Musician) musician1;
-        Musician m2 = (Musician) musician2;
+
 
         try {
+            Musician m1 = (Musician) musician1;
+            Musician m2 = (Musician) musician2;
+
             if (m1.getSkill() > m2.getSkill()) {
                 return 1;
             } else if (m1.getSkill() < m2.getSkill()) {
